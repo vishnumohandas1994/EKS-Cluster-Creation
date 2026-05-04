891fc6689e4135a5f14e831bff73fab4c20a5985
vishnumohandas1994 <100884748+vishnumohandas1994@users.noreply.github.com>
2026-05-04 09:58:19 +0530

    Add files via upload

---
+++ c1_versions.tf
@@ -0,0 +1,26 @@
+terraform {
+  # Minimum Terraform CLI version required
+  required_version = ">= 1.12.0"
+
+  # Required providers and version constraints
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 6.0"
+    }
+  }
+
+  # Remote backend configuration using S3 
+  backend "s3" {
+    bucket         = "tfstate-dev-us-east-1-jpjtof"         
+    key            = "eks/dev/terraform.tfstate"            
+    region         = "us-east-1"                            
+    encrypt        = true                                   
+    use_lockfile   = true     
+  }
+}
+
+provider "aws" {
+  # AWS region to use for all resources (from variables)
+  region = var.aws_region
+}
